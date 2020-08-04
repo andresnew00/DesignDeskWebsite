74096665df857315056bd4ed433e0cf60bf6ff87
andresnew00 <andresnew00@hotmail.com>
2020-08-04 17:44:45 -0400

    Updates

---
+++ precache-manifest.dfe1eb0c60347297451bab84caf05914.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "095b8195442454e8521a0e5f805a00b3",
+    "url": "/DesignDeckWebsite/index.html"
+  },
+  {
+    "revision": "3d3ea3983f2f6bb3289a",
+    "url": "/DesignDeckWebsite/static/css/main.e918919d.chunk.css"
+  },
+  {
+    "revision": "0847cef42ede8636099e",
+    "url": "/DesignDeckWebsite/static/js/2.aa816ef3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DesignDeckWebsite/static/js/2.aa816ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3ea3983f2f6bb3289a",
+    "url": "/DesignDeckWebsite/static/js/main.c214f3ca.chunk.js"
+  },
+  {
+    "revision": "7c93ffe9dae2da071498",
+    "url": "/DesignDeckWebsite/static/js/runtime-main.cf8372ac.js"
+  },
+  {
+    "revision": "8fbed1f415bc1a765538937a94a8bbe2",
+    "url": "/DesignDeckWebsite/static/media/computer-girls.8fbed1f4.svg"
+  },
+  {
+    "revision": "6cb419fb9c986da9ee0aeedc8a56ffdb",
+    "url": "/DesignDeckWebsite/static/media/facebook-icon.6cb419fb.svg"
+  },
+  {
+    "revision": "7f4fad7228a0d1cc1a0c5c6be5841e6a",
+    "url": "/DesignDeckWebsite/static/media/girl-on-laptop.7f4fad72.svg"
+  },
+  {
+    "revision": "1355df0d9de615f55fe1f16a60712d00",
+    "url": "/DesignDeckWebsite/static/media/guy-on-computer.1355df0d.svg"
+  },
+  {
+    "revision": "34df07f044e28e1060f6871b92aedabb",
+    "url": "/DesignDeckWebsite/static/media/guy-searching.34df07f0.svg"
+  },
+  {
+    "revision": "f8c146b1408bdb91378d84929b6276a7",
+    "url": "/DesignDeckWebsite/static/media/guys-coffee.f8c146b1.svg"
+  },
+  {
+    "revision": "034caef7c76fb0fe11bb7646dc4efd81",
+    "url": "/DesignDeckWebsite/static/media/instagram-icon.034caef7.svg"
+  },
+  {
+    "revision": "536f107981fc5241e69e31168a921837",
+    "url": "/DesignDeckWebsite/static/media/logo.536f1079.svg"
+  },
+  {
+    "revision": "649f68cf9028db21b3abee294b299823",
+    "url": "/DesignDeckWebsite/static/media/open-menu.649f68cf.svg"
+  },
+  {
+    "revision": "a87be21d38ea825ec769572e68521327",
+    "url": "/DesignDeckWebsite/static/media/shape-bottom-left.a87be21d.svg"
+  },
+  {
+    "revision": "b3ef35e76e80f6dab5e65c94f231306e",
+    "url": "/DesignDeckWebsite/static/media/shape-bottom-right.b3ef35e7.svg"
+  },
+  {
+    "revision": "81e74c1f482b6ffd33a220bfc2856219",
+    "url": "/DesignDeckWebsite/static/media/shape-top.81e74c1f.svg"
+  }
+]);
