d5626edce2911785cf2d215651e45ce316d5d461
andresnew00 <andresnew00@hotmail.com>
2020-08-11 13:09:18 -0400

    added transitions between pages

--- src/App.js
+++ src/App.js
@@ -3,19 +3,42 @@ import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Navegation from "./pages/Navigation";
-import Contact from "./pages/Contact"
+import Contact from "./pages/Contact";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route path={`${process.env.PUBLIC_URL}/`} exact component={Home} />
-          <Route path={`${process.env.PUBLIC_URL}/navigation`} exact component={Navegation} />
-          <Route path={`${process.env.PUBLIC_URL}/about`} component={About} />
-          <Route path={`${process.env.PUBLIC_URL}/contact`} component={Contact} />
-        </Switch>
+        <Route
+          render={({ location }) => (
+            <TransitionGroup>
+              <CSSTransition key={location.key} timeout={300} classNames="fade">
+                <Switch location={location}>
+                  <Route
+                    path={`${process.env.PUBLIC_URL}/`}
+                    exact
+                    component={Home}
+                  />
+                  <Route
+                    path={`${process.env.PUBLIC_URL}/navigation`}
+                    exact
+                    component={Navegation}
+                  />
+                  <Route
+                    path={`${process.env.PUBLIC_URL}/about`}
+                    component={About}
+                  />
+                  <Route
+                    path={`${process.env.PUBLIC_URL}/contact`}
+                    component={Contact}
+                  />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        />
       </div>
     </Router>
   );
