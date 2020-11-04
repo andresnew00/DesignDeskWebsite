7d9ae032d231cfc9373de20f1d48a1187c741e3b
andresnew00 <andresnew00@hotmail.com>
2020-11-04 15:54:19 -0500

    added new project to portfolio -doctor tires

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function PortfolioItem(props) {
   return (
-    <a className="portfolioItem-wrapper" href={props.link}>
+    <a className="portfolioItem-wrapper" href={props.link} target="_blank">
         <img src={props.image} alt={props.title}/>
     </a>
   );
--- src/components/PortfolioWebsites.js
+++ src/components/PortfolioWebsites.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PortfolioItem from '../components/PortfolioItem'
 import instagramimg from "../assets/InstagramFrame.png";
 import instagramimg2 from "../assets/instagram-img2.jpg";
+import instagramimg3 from "../assets/doctortires.png";
 
 export default function PortfolioWebsites() {
     return (
@@ -9,6 +10,7 @@ export default function PortfolioWebsites() {
             {/* add new projects here by adding title, link and image to a new portfolioItem */}
             <PortfolioItem title="Sunshade Interiors" link={"https://sunshadeinteriors.com/"} image={instagramimg} />
             <PortfolioItem title="B&V Home Maintenance" link={"http://www.bandvhomemaintenance.com/"} image={instagramimg2} />
+            <PortfolioItem title="Doctor Tires" link={"https://doctortires.com/"} image={instagramimg3} />
         </div>
     )
 }
