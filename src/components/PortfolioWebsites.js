fcbb8456a9b985ea05bcd29371e1892491d98c74
Andres Inciarte <you@example.com>
2021-03-17 20:17:11 -0400

    added new item to portfolio pdfsnake

--- src/components/PortfolioWebsites.js
+++ src/components/PortfolioWebsites.js
@@ -3,6 +3,7 @@ import PortfolioItem from '../components/PortfolioItem'
 import instagramimg from "../assets/InstagramFrame.png";
 import instagramimg2 from "../assets/instagram-img2.jpg";
 import instagramimg3 from "../assets/doctortires.png";
+import instagramimg4 from "../assets/Instagrampost-pdfsnake.png";
 
 export default function PortfolioWebsites() {
     return (
@@ -11,6 +12,7 @@ export default function PortfolioWebsites() {
             <PortfolioItem title="Sunshade Interiors" link={"https://sunshadeinteriors.com/"} image={instagramimg} />
             <PortfolioItem title="B&V Home Maintenance" link={"http://www.bandvhomemaintenance.com/"} image={instagramimg2} />
             <PortfolioItem title="Doctor Tires" link={"https://doctortires.com/"} image={instagramimg3} />
+            <PortfolioItem title="PDFSnake" link={"https://pdfsnake.app/"} image={instagramimg4} />
         </div>
     )
 }
